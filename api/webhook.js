a3ddde788001b49cf4ee9158108d73168cd0ac4b
zszoro <zszoro818@gmail.com>
2026-04-20 00:37:08 -0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,12 +1,32 @@
 export default async function handler(req, res) {
     try {
-        console.log("🔔 Webhook recebido:", req.method, req.body);
+        const paymentId = req.body?.data?.id;
 
-        // Sempre responde OK (isso faz o teste passar)
+        if (!paymentId) {
+            return res.status(200).json({ ok: true });
+        }
+
+        const response = await fetch(
+            `https://api.mercadopago.com/v1/payments/${paymentId}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+                }
+            }
+        );
+
+        const payment = await response.json();
+
+        console.log("Status:", payment.status);
+
+        if (payment.status === "approved") {
+            console.log("✅ PAGAMENTO APROVADO");
+        }
+
         return res.status(200).json({ ok: true });
 
     } catch (error) {
-        console.error("Erro:", error);
+        console.error(error);
         return res.status(200).json({ ok: true });
     }
 }
