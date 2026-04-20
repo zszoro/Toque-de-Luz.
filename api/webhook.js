1e7f02700f8ce652c1d9e4c0905bbca83055d667
zszoro <zszoro818@gmail.com>
2026-04-20 00:26:36 -0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,15 +1,1 @@
-export default async function handler(req, res) {
-    if (req.method !== "POST") {
-        return res.status(200).json({ ok: true });
-    }
 
-    try {
-        console.log("🔔 Webhook recebido:", req.body);
-
-        return res.status(200).json({ ok: true });
-
-    } catch (error) {
-        console.error(error);
-        return res.status(200).json({ ok: true });
-    }
-}
